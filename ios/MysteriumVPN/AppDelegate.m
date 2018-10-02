cd7d0723e81e4435e87912bebee6fe7dda68c583
Arnas Dundulis <Arrnas@users.noreply.github.com>
2018-10-02 19:06:50 +0300

    Add firebase and crashlytics to ios project

--- ios/MysteriumVPN/AppDelegate.m
+++ ios/MysteriumVPN/AppDelegate.m
@@ -10,6 +10,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import Firebase;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -29,6 +31,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 
