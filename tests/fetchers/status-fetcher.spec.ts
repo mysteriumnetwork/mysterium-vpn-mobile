319a7f6b8bec12774fbe4f99de65721222755032
mipo47 <mipo3815@gmail.com>
2018-10-08 17:18:20 +0300

    Enable Jest unit testing, add tiny example

---
+++ tests/fetchers/status-fetcher.spec.ts
@@ -0,0 +1,26 @@
+import { StatusFetcher } from '../../js/fetchers/status-fetcher'
+import { TequilapiClientMock } from '../mocks/tequilapi-mock'
+import { TequilapiClient } from 'mysterium-tequilapi'
+import {store} from "../../js/store/tequilapi-store"
+
+describe('StatusFetcher', () => {
+  let api: TequilapiClient
+  let fetcher: StatusFetcher
+
+  beforeEach(() => {
+    api = new TequilapiClientMock()
+    fetcher = new StatusFetcher(api)
+  })
+
+  test('.constructor', async () => {
+    expect(api.connectionStatus).toHaveBeenCalled()
+  })
+
+  test('.refresh', async () => {
+    await fetcher.refresh()
+    expect(store.ConnectionStatus).toEqual({
+      status: 'NotConnected',
+      sessionId: 'MOCKED_SESSION_ID'
+    })
+  })
+})
