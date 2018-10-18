420384a0ee8883de15609870c48e3031036a294c
Donatas Kucinskas <donce.lt@gmail.com>
2018-10-18 09:11:41 +0300

    Add object-curly-spaces rule, unify spacing in code

--- js/store/app-store.ts
+++ js/store/app-store.ts
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {computed, observable} from 'mobx'
+import { computed, observable } from 'mobx'
 import {
   ConnectionStatisticsDTO,
   ConnectionStatus,
