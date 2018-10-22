a247edd84344427a74ab12b93f910cfe5476a701
mipo47 <mipo3815@gmail.com>
2018-10-22 10:56:48 +0300

    group tests

--- tests/fetchers/status-fetcher.spec.ts
+++ tests/fetchers/status-fetcher.spec.ts
@@ -24,60 +24,68 @@ describe('StatusFetcher', () => {
     jest.runAllTicks()
   })
 
-  it('.constructor',  () => {
-    expect(fetcher.isStarted).toBe(true)
+  describe('.constructor', () => {
+    it('starts continuous status fetching', () => {
+      expect(fetcher.isStarted).toBe(true)
 
-    expect(api.connectionStatus).toHaveBeenCalledTimes(1)
-    expect(store.ConnectionStatus).toEqual({
-      status: 'NotConnected',
-      sessionId: 'MOCKED_SESSION_ID',
-    })
-    expect(fetcher.isRunning).toBe(false)
-
-    for (let call = 2; call < 6; call++) {
-      jest.advanceTimersByTime(refreshInterval)
-      expect(api.connectionStatus).toHaveBeenCalledTimes(call)
-      jest.runAllTicks()
+      expect(api.connectionStatus).toHaveBeenCalledTimes(1)
+      expect(store.ConnectionStatus).toEqual({
+        status: 'NotConnected',
+        sessionId: 'MOCKED_SESSION_ID',
+      })
       expect(fetcher.isRunning).toBe(false)
-    }
+
+      for (let call = 2; call < 6; call++) {
+        jest.advanceTimersByTime(refreshInterval)
+        expect(api.connectionStatus).toHaveBeenCalledTimes(call)
+        jest.runAllTicks()
+        expect(fetcher.isRunning).toBe(false)
+      }
+    })
   })
 
-  it('.start', () => {
-    fetcher.stop()
-    jest.runAllTicks()
-    expect(api.connectionStatus).toHaveBeenCalledTimes(1)
-    expect(fetcher.isRunning).toBe(false)
+  describe('.start', () => {
+    it('starts continuous status fetching', () => {
+      fetcher.stop()
+      jest.runAllTicks()
+      expect(api.connectionStatus).toHaveBeenCalledTimes(1)
+      expect(fetcher.isRunning).toBe(false)
 
-    fetcher.start(refreshInterval)
-    expect(api.connectionStatus).toHaveBeenCalledTimes(2)
+      fetcher.start(refreshInterval)
+      expect(api.connectionStatus).toHaveBeenCalledTimes(2)
+    })
   })
 
-  it('.stop', () => {
-    expect(api.connectionStatus).toHaveBeenCalledTimes(1)
-    jest.runTimersToTime(refreshInterval)
-    expect(api.connectionStatus).toHaveBeenCalledTimes(2)
+  describe('.stop', () => {
+    it('stops fetching status', () => {
+      expect(api.connectionStatus).toHaveBeenCalledTimes(1)
+      jest.runTimersToTime(refreshInterval)
+      expect(api.connectionStatus).toHaveBeenCalledTimes(2)
 
-    fetcher.stop()
-    expect(fetcher.isStarted).toBe(false)
+      fetcher.stop()
+      expect(fetcher.isStarted).toBe(false)
 
-    jest.runTimersToTime(refreshInterval)
-    expect(api.connectionStatus).toHaveBeenCalledTimes(2)
-    jest.runTimersToTime(refreshInterval)
-    expect(api.connectionStatus).toHaveBeenCalledTimes(2)
+      jest.runTimersToTime(refreshInterval)
+      expect(api.connectionStatus).toHaveBeenCalledTimes(2)
+      jest.runTimersToTime(refreshInterval)
+      expect(api.connectionStatus).toHaveBeenCalledTimes(2)
+    })
   })
 
-  it('.refresh', async () => {
-    expect(fetcher.isRunning).toBe(false)
-    store.ConnectionStatus = undefined
+  describe('.refresh', () => {
+    it('fetches status immediately', async () => {
+      expect(fetcher.isRunning).toBe(false)
+      store.ConnectionStatus = undefined
 
-    const connectedStatus = {
-      status: 'Connected',
-      sessionId: 'MOCKED_SESSION_ID',
-    }
-    api.connectionStatus = jest.fn()
-      .mockReturnValue(connectedStatus)
+      const connectedStatus = {
+        status: 'Connected',
+        sessionId: 'MOCKED_SESSION_ID',
+      }
+      api.connectionStatus = jest.fn()
+        .mockReturnValue(connectedStatus)
 
-    await fetcher.refresh()
-    expect(store.ConnectionStatus).toEqual(connectedStatus)
+      await fetcher.refresh()
+      expect(store.ConnectionStatus).toEqual(connectedStatus)
+    })
   })
 })
