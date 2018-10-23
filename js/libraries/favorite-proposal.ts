29bbb74b4f140333bec2fc64357859a0a5cce6f3
Arnas Dundulis <Arrnas@users.noreply.github.com>
2018-10-23 19:16:07 +0300

    Rename store-proposals -> storage

--- js/libraries/favorite-proposal.ts
+++ js/libraries/favorite-proposal.ts
@@ -18,7 +18,7 @@
 import { ProposalDTO } from 'mysterium-tequilapi'
 import { CONFIG } from '../config'
 import { Countries } from './countries'
-import { storage } from './favorite-storage'
+import { storage } from './storage'
 
 class Proposal {
   public static compare (
