5b8129f0c875793497830f1804c0505bf84b3f57
Arnas Dundulis <Arrnas@users.noreply.github.com>
2018-10-26 19:24:58 +0300

    Add initial network extension

---
+++ ios/MysteriumNetworkExtension/PacketTunnelProvider.swift
@@ -0,0 +1,15 @@
+//
+//  PacketTunnelProvider.swift
+//  MysteriumNetworkExtension
+//
+//  Created by Arnas Dundulis on 26/10/2018.
+//  Copyright © 2018 Mysterium Network. All rights reserved.
+//
+
+import NetworkExtension
+
+class PacketTunnelProvider: NEPacketTunnelProvider {
+    
+    override func wake() {
+    }
+}
