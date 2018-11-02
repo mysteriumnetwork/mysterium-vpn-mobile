93ac265394ce72350f6ae15bcd86c0ee0b3868ac
Donatas Kucinskas <donce.lt@gmail.com>
2018-11-02 17:35:09 +0200

    Add specs for ConnectionStatus

---
+++ tests/unit/app/components/connection-status.spec.tsx
@@ -0,0 +1,27 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import ConnectionStatus from '../../../../src/app/components/connection-status'
+import { ConnectionStatusEnum } from '../../../../src/libraries/tequilAPI/enums'
+
+describe('ConnectionStatus', () => {
+  it('renders loading text with no status', () => {
+    const wrapper = shallow(
+      <ConnectionStatus status={undefined}/>
+    )
+    expect(wrapper.childAt(0).text()).toEqual('Loading...')
+  })
+
+  it('renders custom text with known status', () => {
+    const wrapper = shallow(
+      <ConnectionStatus status={ConnectionStatusEnum.NOT_CONNECTED}/>
+    )
+    expect(wrapper.childAt(0).text()).toEqual('Disconnected')
+  })
+
+  it('throws error with unknown status', () => {
+    const render = () => shallow(
+      <ConnectionStatus status={'something'}/>
+    )
+    expect(render).toThrowError('Unknown connection status: something')
+  })
+})
