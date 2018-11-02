76a7316d58ec8a297e8535e947500c339542f00b
Ignas Bernotas <ignas@iber.lt>
2018-11-02 13:19:01 +0200

    Removed android e2e command, minor fixes

--- tests/e2e/screens/home-screen.spec.js
+++ tests/e2e/screens/home-screen.spec.js
@@ -3,7 +3,7 @@ describe('HomeScreen', () => {
     await device.reloadReactNative()
   })
 
-  it('Should show IP updating status', () => {
+  it('should show IP updating status', () => {
     expect(element(by.text('IP: updating...'))).toExist()
   })
 })
