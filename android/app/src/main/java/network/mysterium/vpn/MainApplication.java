704b10d0c8152dbaf00a58de04d1684a4ef7d3ac
Andrej Novikov <jazzaiman@gmail.com>
2018-12-03 10:37:15 +0200

    run react-native install react-native-fabric
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- android/app/src/main/java/network/mysterium/vpn/MainApplication.java
+++ android/app/src/main/java/network/mysterium/vpn/MainApplication.java
@@ -3,6 +3,7 @@ package network.mysterium.vpn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.smixx.fabric.FabricPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+            new FabricPackage(),
               new VectorIconsPackage()
       );
     }
