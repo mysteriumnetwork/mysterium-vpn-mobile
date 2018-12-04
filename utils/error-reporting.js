d16b866179e968b9964263f14880e4f53f1e20a8
Andrej Novikov <jazzaiman@gmail.com>
2018-12-04 10:50:09 +0200

    add Crashlytics to error-reporting.js
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- utils/error-reporting.js
+++ utils/error-reporting.js
@@ -1,8 +1,14 @@
+import { Crashlytics } from 'react-native-fabric'
 
 const setupErrorHandlers = () => {
   const defaultHandler = ErrorUtils.getGlobalHandler()
   const wrapGlobalHandler = async (error, isFatal) => {
-    // custom error handling could be added here.
+    Crashlytics.setBool('dev', true)
+    // following Android only
+    Crashlytics.logException(error.message);
+    // ios only
+    Crashlytics.recordError(error)
+
     defaultHandler(error, isFatal)
   }
   ErrorUtils.setGlobalHandler(wrapGlobalHandler)
