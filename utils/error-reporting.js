cd5c209818309fbf67b94338a888e00ab2926e13
Andrej Novikov <jazzaiman@gmail.com>
2018-12-06 14:04:38 +0200

    removing dev key and ios fabric sending
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- utils/error-reporting.js
+++ utils/error-reporting.js
@@ -3,11 +3,8 @@ import { Crashlytics } from 'react-native-fabric'
 const setupErrorHandlers = () => {
   const defaultHandler = ErrorUtils.getGlobalHandler()
   const wrapGlobalHandler = async (error, isFatal) => {
-    Crashlytics.setBool('dev', true)
     // following Android only
     Crashlytics.logException(error.message);
-    // ios only
-    Crashlytics.recordError(error)
 
     defaultHandler(error, isFatal)
   }
