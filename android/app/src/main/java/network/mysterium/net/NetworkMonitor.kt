d4f0f8998e0e6c438bd7baed4d4041ac3c418660
Tadas Krivickas <tadas.krivickas@gmail.com>
2020-07-29 15:09:10 +0300

    Improve network switch handling

--- android/app/src/main/java/network/mysterium/net/NetworkMonitor.kt
+++ android/app/src/main/java/network/mysterium/net/NetworkMonitor.kt
@@ -1,7 +1,10 @@
 package network.mysterium.net
 
-import android.net.*
+import android.net.ConnectivityManager
+import android.net.Network
 import android.net.NetworkCapabilities.*
+import android.net.NetworkInfo.DetailedState.CONNECTED
+import android.net.NetworkInfo.DetailedState.OBTAINING_IPADDR
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
 import android.util.Log
@@ -10,79 +13,99 @@ import kotlinx.coroutines.*
 
 class NetworkMonitor(
         private val connectivity: ConnectivityManager,
-        private val wifi: WifiManager
+        private val wifiManager: WifiManager,
+        private val networkState: MutableLiveData<NetworkState>
+
 ) : ConnectivityManager.NetworkCallback() {
 
     companion object {
         private const val TAG = "NetworkMonitor"
     }
 
-    val state = MutableLiveData(NetworkState())
+    private var networkCallback: ConnectivityManager.NetworkCallback? = null
+    private var activeNetwork: Network? = null
+    private var delayedNetworkJoin: Job? = null
+    private var delayedNetworkLoss: Job? = null
+    private val networkStateChangeDelay = 7_000L
 
-    fun startWatching() {
-        connectivity.registerNetworkCallback(NetworkRequest.Builder()
-                .addTransportType(TRANSPORT_WIFI)
-                .addTransportType(TRANSPORT_CELLULAR)
-                .build(),
-                this
-        )
+    fun start() {
+        networkCallback = NetworkStatePublisher()
+        connectivity.registerDefaultNetworkCallback(networkCallback)
     }
 
-    override fun onCapabilitiesChanged(network: Network?, networkCapabilities: NetworkCapabilities?) {
-        Log.d(TAG, "Network capabilities changed, refreshing state $networkCapabilities")
-        refreshNetworkState()
+    fun stop() {
+        networkCallback?.let { connectivity.unregisterNetworkCallback(it) }
+        networkCallback = null
     }
 
-    override fun onLost(network: Network?) {
-        Log.d(TAG, "Lost a network, refreshing state")
-        refreshNetworkState()
-    }
+    inner class NetworkStatePublisher : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network?) {
+            if (network == null) {
+                return
+            }
+            val networkInfo = connectivity.getNetworkInfo(network) ?: return
+            val capabilities = connectivity.getNetworkCapabilities(network) ?: return
 
-    private fun refreshNetworkState() {
-        val connectedNetworks = connectivity.allNetworks.filter { connectivity.getNetworkInfo(it)?.isConnected == true }
-        val wifiNetwork = connectedNetworks.find {
-            val cap = connectivity.getNetworkCapabilities(it) ?: return@find false
-            return@find cap.hasTransport(TRANSPORT_WIFI)
-                    && cap.hasCapability(NET_CAPABILITY_INTERNET)
-                    && cap.hasCapability(NET_CAPABILITY_VALIDATED)
-        }
-        val cellularNetwork = connectedNetworks.find {
-            val cap = connectivity.getNetworkCapabilities(it) ?: return@find false
-            return@find cap.hasTransport(TRANSPORT_CELLULAR)
-                    && cap.hasCapability(NET_CAPABILITY_INTERNET)
-                    && cap.hasCapability(NET_CAPABILITY_VALIDATED)
-        }
-        val newState = when {
-            wifiNetwork != null -> {
-                NetworkState(wifiConnected = true, wifiNetworkId = getWifiNetworkId(this.wifi))
+            val newState = when {
+                capabilities.hasTransport(TRANSPORT_VPN) -> return
+                capabilities.hasTransport(TRANSPORT_WIFI) -> NetworkState(wifiConnected = true, wifiNetworkId = getWifiNetworkId())
+                capabilities.hasTransport(TRANSPORT_CELLULAR) -> NetworkState(cellularConnected = true)
+                else -> NetworkState()
             }
-            cellularNetwork != null -> {
-                NetworkState(cellularConnected = true)
-            }
-            else -> {
-                NetworkState()
-            }
-        }
 
-        if (newState != state.value) {
-            CoroutineScope(Dispatchers.Main).launch {
-                Log.i(TAG, "Network state changed: ${state.value} -> $newState")
-                state.postValue(newState)
+            if (activeNetwork != network) {
+                delayedNetworkLoss?.let {
+                    Log.i(TAG, "Network connectivity restored, canceling network loss")
+                    it.cancel()
+                    delayedNetworkLoss = null
+                }
+                delayedNetworkJoin?.let {
+                    Log.i(TAG, "Connected to another network, canceling network join")
+                    it.cancel()
+                    delayedNetworkJoin = null
+                }
+                delayedNetworkJoin = CoroutineScope(Dispatchers.Main).launch {
+                    delay(if (newState.wifiConnected) 0 else networkStateChangeDelay)
+                    Log.i(TAG, "Network joined: ${transportType(network)} ${networkInfo.detailedState}")
+                    networkState.value = newState
+                    activeNetwork = network
+                    delayedNetworkJoin = null
+                }
             }
         }
-    }
 
-}
-
-fun getWifiNetworkId(manager: WifiManager): Int {
-    if (manager.isWifiEnabled) {
-        val wifiInfo = manager.connectionInfo
-        if (wifiInfo != null) {
-            val state = WifiInfo.getDetailedStateOf(wifiInfo.supplicantState)
-            if (state == NetworkInfo.DetailedState.CONNECTED || state == NetworkInfo.DetailedState.OBTAINING_IPADDR) {
-                return wifiInfo.networkId
+        override fun onLost(network: Network?) {
+            if (connectivity.activeNetworkInfo?.isConnected != true) {
+                delayedNetworkLoss = CoroutineScope(Dispatchers.Main).launch {
+                    delay(networkStateChangeDelay)
+                    Log.i(TAG, "Network lost: ${transportType(network)}")
+                    networkState.value = NetworkState()
+                    delayedNetworkLoss = null
+                }
             }
         }
     }
-    return 0
+
+    private fun transportType(network: Network?): String {
+        val cap = network?.let { connectivity.getNetworkCapabilities(it) } ?: return "UNKNOWN"
+        return when {
+            cap.hasTransport(TRANSPORT_WIFI) -> "WIFI"
+            cap.hasTransport(TRANSPORT_CELLULAR) -> "CELLULAR"
+            else -> "UNKNOWN"
+        }
+    }
+
+    private fun getWifiNetworkId(): Int {
+        if (wifiManager.isWifiEnabled) {
+            val wifiInfo = wifiManager.connectionInfo
+            if (wifiInfo != null) {
+                val state = WifiInfo.getDetailedStateOf(wifiInfo.supplicantState)
+                if (state in listOf(CONNECTED, OBTAINING_IPADDR)) {
+                    return wifiInfo.networkId
+                }
+            }
+        }
+        return 0
+    }
+
 }
