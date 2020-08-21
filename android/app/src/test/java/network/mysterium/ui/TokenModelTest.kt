5c1f5d5512f967e69256e88757f6929d8d69f20c
Dmitry Shihovtsev <soffokulus@gmail.com>
2020-08-21 09:00:54 +0600

    Fix unit tests for v3 payments

--- android/app/src/test/java/network/mysterium/ui/TokenModelTest.kt
+++ android/app/src/test/java/network/mysterium/ui/TokenModelTest.kt
@@ -6,17 +6,17 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 
 @RunWith(Parameterized::class)
-class TokenModelTest(private val token: Long, private val display: String) {
+class TokenModelTest(private val token: Double, private val display: String) {
 
     companion object {
         @JvmStatic
         @Parameterized.Parameters
         fun data(): Collection<Array<Any>> {
             return listOf(
-                    arrayOf(124_00_000_000L, "124.000 MYSTT"),
-                    arrayOf(124_54_000_000L, "124.540 MYSTT"),
-                    arrayOf(114_34_000_000L, "114.340 MYSTT"),
-                    arrayOf(123_01_000_000L, "123.010 MYSTT")
+                    arrayOf(124.000, "124.000 MYSTT"),
+                    arrayOf(124.540, "124.540 MYSTT"),
+                    arrayOf(114.340, "114.340 MYSTT"),
+                    arrayOf(123.010, "123.010 MYSTT")
             )
         }
     }
