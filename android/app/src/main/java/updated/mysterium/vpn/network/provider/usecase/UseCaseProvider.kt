52ef670e7a9f07f7fcd90690439754f0ed6e2642
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2021-03-01 23:05:56 +0200

    MN-53_54_55 - Fix bugs for MN-53, MN-54 and MN-55

--- android/app/src/main/java/updated/mysterium/vpn/network/provider/usecase/UseCaseProvider.kt
+++ android/app/src/main/java/updated/mysterium/vpn/network/provider/usecase/UseCaseProvider.kt
@@ -1,14 +1,17 @@
 package updated.mysterium.vpn.network.provider.usecase
 
+import android.content.Context
 import network.mysterium.service.core.NodeRepository
 import updated.mysterium.vpn.database.AppDatabase
 import updated.mysterium.vpn.network.usecase.ConnectionUseCase
 import updated.mysterium.vpn.network.usecase.LocationUseCase
+import updated.mysterium.vpn.network.usecase.LoginUseCase
 import updated.mysterium.vpn.network.usecase.NodesUseCase
 
 class UseCaseProvider(
     private val nodeRepository: NodeRepository,
-    private val database: AppDatabase
+    private val database: AppDatabase,
+    private val context: Context
 ) {
 
     fun nodes() = NodesUseCase(nodeRepository, database.nodeDao())
@@ -16,4 +19,6 @@ class UseCaseProvider(
     fun location() = LocationUseCase(nodeRepository)
 
     fun connection() = ConnectionUseCase(nodeRepository)
+
+    fun login() = LoginUseCase(context)
 }
