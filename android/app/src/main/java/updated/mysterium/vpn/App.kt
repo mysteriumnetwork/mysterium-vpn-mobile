c72634bd731f15b25ea05ce1c5e9411f672116ca
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2021-03-10 19:15:51 +0200

    MN-47 Move keys to GradleConfig

--- android/app/src/main/java/updated/mysterium/vpn/App.kt
+++ android/app/src/main/java/updated/mysterium/vpn/App.kt
@@ -3,16 +3,12 @@ package updated.mysterium.vpn
 import android.app.Application
 import io.intercom.android.sdk.Intercom
 import network.mysterium.ui.Countries
+import network.mysterium.vpn.BuildConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import updated.mysterium.vpn.di.Modules
 
 class App : Application() {
-
-    private companion object {
-        const val API_KEY = "android_sdk-e480f3fce4f2572742b13c282c453171c1715516"
-        const val APP_ID = "h7hlm9on"
-    }
 
     override fun onCreate() {
         super.onCreate()
@@ -25,6 +21,10 @@ class App : Application() {
     }
 
     private fun setupIntercom() {
-        Intercom.initialize(this, API_KEY, APP_ID)
+        Intercom.initialize(
+            this,
+            BuildConfig.INTERCOM_API_KEY,
+            BuildConfig.INTERCOM_APP_ID
+        )
     }
 }
