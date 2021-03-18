86218c001a450e264123c30ed6e8336e83d3a375
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2021-03-18 15:17:19 +0200

    Fix bugs with Chart and response data

--- android/app/src/main/java/updated/mysterium/vpn/network/usecase/StatisticUseCase.kt
+++ android/app/src/main/java/updated/mysterium/vpn/network/usecase/StatisticUseCase.kt
@@ -9,12 +9,18 @@ import updated.mysterium.vpn.model.session.Session
 
 class StatisticUseCase(private val nodeRepository: NodeRepository) {
 
+    private companion object {
+        const val CURRENT_CONNECTION = "0001-01-01T00:00:00Z"
+    }
+
     suspend fun getLastSessions(): List<Session> {
         val jsonResponse = nodeRepository.getLastSessions(SessionFilter())
         val collectionType = object : TypeToken<Collection<Session?>?>() {}.type
         val sessions: List<Session> = Gson().fromJson(String(jsonResponse), collectionType)
         return sessions.filter { // Get sessions only for last seven days
             DateUtil.dateDiffInDaysFromCurrent(it.timeStarted) < 7
+        }.filter { // Not add current connection
+            it.timeUpdated != CURRENT_CONNECTION
         }
     }
 }
