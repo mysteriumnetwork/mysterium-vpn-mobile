c272be91268053687d7877025a97b54c4910277d
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2021-03-24 13:33:23 +0200

    Add value chooser

--- android/app/src/main/java/updated/mysterium/vpn/di/Modules.kt
+++ android/app/src/main/java/updated/mysterium/vpn/di/Modules.kt
@@ -21,6 +21,7 @@ import updated.mysterium.vpn.ui.profile.ProfileViewModel
 import updated.mysterium.vpn.ui.report.issue.ReportIssueViewModel
 import updated.mysterium.vpn.ui.splash.SplashViewModel
 import updated.mysterium.vpn.ui.terms.TermsOfUseViewModel
+import updated.mysterium.vpn.ui.top.up.amount.TopUpAmountViewModel
 import updated.mysterium.vpn.ui.wallet.WalletViewModel
 import updated.mysterium.vpn.ui.wallet.spendings.SpendingsViewModel
 
@@ -81,6 +82,9 @@ object Modules {
         }
         viewModel {
             TermsOfUseViewModel(get())
+        }
+        viewModel {
+            TopUpAmountViewModel(get())
         }
     }
 
--- android/app/src/main/java/updated/mysterium/vpn/ui/top/up/amount/TopUpAmountActivity.kt
+++ android/app/src/main/java/updated/mysterium/vpn/ui/top/up/amount/TopUpAmountActivity.kt
@@ -1,23 +1,66 @@
 package updated.mysterium.vpn.ui.top.up.amount
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import network.mysterium.vpn.R
 import network.mysterium.vpn.databinding.ActivityTopUpAmountBinding
+import org.koin.android.ext.android.inject
+import updated.mysterium.vpn.model.top.up.TopUpCardItem
 
 class TopUpAmountActivity : AppCompatActivity() {
 
+    private companion object {
+        const val TAG = "TopUpAmountActivity"
+        val AMOUNT_VALUES = listOf(
+            TopUpCardItem("5", true),
+            TopUpCardItem("10"),
+            TopUpCardItem("15"),
+            TopUpCardItem("20"),
+            TopUpCardItem("25")
+        )
+    }
+
     private lateinit var binding: ActivityTopUpAmountBinding
+    private val viewModel: TopUpAmountViewModel by inject()
+    private val topUpAdapter = TopUpAmountAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTopUpAmountBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        configure()
         bindsAction()
+    }
+
+    private fun configure() {
+        binding.amountRecycler.adapter = topUpAdapter
+        topUpAdapter.replaceAll(AMOUNT_VALUES)
+        topUpAdapter.onItemSelected = {
+            updateEquivalent(it.value.toInt())
+        }
+        updateEquivalent(AMOUNT_VALUES.first().value.toInt())
     }
 
     private fun bindsAction() {
         binding.backButton.setOnClickListener {
             finish()
         }
+        binding.confirmButton.setOnClickListener {
+            topUpAdapter.getSelectedValue()
+            // TODO("Get selected value and move forward to next screen")
+        }
+    }
+
+    private fun updateEquivalent(value: Int) {
+        viewModel.getUsdEquivalent(value).observe(this, { result ->
+            result.onSuccess {
+                binding.usdEquivalentTextView.text = getString(R.string.top_up_usd_equivalent, it)
+            }
+            result.onFailure { throwable ->
+                Log.e(TAG, throwable.localizedMessage ?: throwable.toString())
+                // TODO("Add UI error, failed to load equivalent")
+            }
+        })
     }
 }
