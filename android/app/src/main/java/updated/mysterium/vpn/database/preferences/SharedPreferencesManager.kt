bf9b7dd767c22d8f3c209733e2612ac3203fa357
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2021-03-29 14:02:14 +0300

    Remove comments

--- android/app/src/main/java/updated/mysterium/vpn/database/preferences/SharedPreferencesManager.kt
+++ android/app/src/main/java/updated/mysterium/vpn/database/preferences/SharedPreferencesManager.kt
@@ -4,56 +4,6 @@ import android.content.Context
 
 class SharedPreferencesManager(private val context: Context) {
 
-//    private companion object {
-//        const val BALANCE_POP_UP_PREFERENCES_KEY = "BALANCE_POP_UP"
-//        const val BALANCE_POP_UP_SHOWN_KEY = "BALANCE_POP_UP_SHOWN"
-//        const val LOGIN_PREFERENCES_KEY = "LOGIN"
-//        const val ALREADY_LOGIN_KEY = "ALREADY_LOGIN"
-//        const val TERMS_PREFERENCES_KEY = "TERMS"
-//        const val ALREADY_ACCEPTED_KEY = "ALREADY_ACCEPTED"
-//    }
-//
-//    fun isBalancePopUpShown() = context.getSharedPreferences(
-//        BALANCE_POP_UP_PREFERENCES_KEY, Context.MODE_PRIVATE
-//    ).contains(BALANCE_POP_UP_SHOWN_KEY)
-//
-//    fun balancePopUpShown() = context
-//        .getSharedPreferences(BALANCE_POP_UP_PREFERENCES_KEY, Context.MODE_PRIVATE)
-//        .edit()
-//        .putBoolean(BALANCE_POP_UP_SHOWN_KEY, true).apply()
-//
-//    fun isAlreadyLogin() = context.getSharedPreferences(LOGIN_PREFERENCES_KEY, Context.MODE_PRIVATE)
-//        .contains(ALREADY_LOGIN_KEY)
-//
-//    fun userLoggedIn() = context.getSharedPreferences(LOGIN_PREFERENCES_KEY, Context.MODE_PRIVATE)
-//        .edit()
-//        .putBoolean(ALREADY_LOGIN_KEY, true).apply()
-//
-//    fun isTermsAccepted() = context.getSharedPreferences(TERMS_PREFERENCES_KEY, Context.MODE_PRIVATE)
-//        .contains(ALREADY_ACCEPTED_KEY)
-//
-//    fun userAcceptTerms() = context.getSharedPreferences(TERMS_PREFERENCES_KEY, Context.MODE_PRIVATE)
-//        .edit()
-//        .putBoolean(ALREADY_ACCEPTED_KEY, true).apply()
-
-
-
-//    fun setPreferenceValue(key: SharedPreferencesList, value: Int) {
-//        context.getSharedPreferences(key.prefName, Context.MODE_PRIVATE)
-//            .edit().let {
-//                it.putInt(key.prefName, value)
-//                it.apply()
-//            }
-//    }
-//
-//    fun setPreferenceValue(key: SharedPreferencesList, value: String) {
-//        context.getSharedPreferences(key.prefName, Context.MODE_PRIVATE)
-//            .edit().let {
-//                it.putString(key.prefName, value)
-//                it.apply()
-//            }
-//    }
-
     fun setPreferenceValue(key: SharedPreferencesList, value: Boolean) {
         context.getSharedPreferences(key.prefName, Context.MODE_PRIVATE)
             .edit().let {
