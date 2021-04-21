566c63d2502d5601272fe7c7c46f7e4e4a3a3304
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2021-04-22 00:12:04 +0300

    Added favourites screen

--- android/app/src/main/java/updated/mysterium/vpn/ui/manual/connect/search/SearchViewModel.kt
+++ android/app/src/main/java/updated/mysterium/vpn/ui/manual/connect/search/SearchViewModel.kt
@@ -3,30 +3,19 @@ package updated.mysterium.vpn.ui.manual.connect.search
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
 import updated.mysterium.vpn.model.manual.connect.Proposal
-import updated.mysterium.vpn.network.provider.usecase.UseCaseProvider
 import java.util.*
 
-class SearchViewModel(useCaseProvider: UseCaseProvider) : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     val searchResult: LiveData<List<Proposal>>
         get() = _searchResult
 
-    val initialDataLoaded: LiveData<Unit>
-        get() = _initialDataLoaded
-
-    private val nodesUseCase = useCaseProvider.nodes()
     private val _searchResult = MutableLiveData<List<Proposal>>()
-    private val _initialDataLoaded = MutableLiveData<Unit>()
     private var allProposal = emptyList<Proposal>()
 
-    init {
-        viewModelScope.launch {
-            allProposal = nodesUseCase.getAllProposals()
-            _initialDataLoaded.postValue(Unit)
-        }
+    fun setAllNodes(proposals: List<Proposal>) {
+        allProposal = proposals
     }
 
     fun search(text: String) {
