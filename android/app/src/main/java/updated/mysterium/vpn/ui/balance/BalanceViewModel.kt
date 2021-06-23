d5be16a8f8005e18dfd8a5aa4ba5e6f6e19b648d
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2021-06-23 11:22:24 +0300

    Updated balance initialisation

--- android/app/src/main/java/updated/mysterium/vpn/ui/balance/BalanceViewModel.kt
+++ android/app/src/main/java/updated/mysterium/vpn/ui/balance/BalanceViewModel.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import mysterium.GetBalanceRequest
-import updated.mysterium.vpn.core.DeferredNode
 import updated.mysterium.vpn.core.MysteriumCoreService
 import updated.mysterium.vpn.model.wallet.IdentityModel
 import updated.mysterium.vpn.model.wallet.IdentityRegistrationStatus
@@ -41,6 +40,14 @@ class BalanceViewModel(useCaseProvider: UseCaseProvider) : ViewModel() {
 
     fun getCurrentBalance() {
         viewModelScope.launch(Dispatchers.IO) {
+            if (balanceRequest == null) {
+                val handler = CoroutineExceptionHandler { _, exception ->
+                    Log.e(TAG, exception.localizedMessage ?: exception.toString())
+                }
+                viewModelScope.launch(Dispatchers.IO + handler) {
+                    initBalanceRequest()
+                }
+            }
             balanceRequest?.let {
                 _balanceLiveData.postValue(balanceUseCase.getBalance(it))
             }
@@ -52,17 +59,21 @@ class BalanceViewModel(useCaseProvider: UseCaseProvider) : ViewModel() {
         val handler = CoroutineExceptionHandler { _, exception ->
             Log.e(TAG, exception.localizedMessage ?: exception.toString())
         }
-        viewModelScope.launch(handler) {
+        viewModelScope.launch(Dispatchers.IO + handler) {
             initBalanceListener()
-            val nodeIdentity = connectionUseCase.getIdentity()
-            val identity = IdentityModel(
-                address = nodeIdentity.address,
-                channelAddress = nodeIdentity.channelAddress,
-                status = IdentityRegistrationStatus.parse(nodeIdentity.registrationStatus)
-            )
-            balanceRequest = GetBalanceRequest().apply {
-                identityAddress = identity.address
-            }
+            initBalanceRequest()
+        }
+    }
+
+    private suspend fun initBalanceRequest() {
+        val nodeIdentity = connectionUseCase.getIdentity()
+        val identity = IdentityModel(
+            address = nodeIdentity.address,
+            channelAddress = nodeIdentity.channelAddress,
+            status = IdentityRegistrationStatus.parse(nodeIdentity.registrationStatus)
+        )
+        balanceRequest = GetBalanceRequest().apply {
+            identityAddress = identity.address
         }
     }
 
