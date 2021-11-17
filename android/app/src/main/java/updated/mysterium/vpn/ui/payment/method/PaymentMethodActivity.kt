082be5279ae9ed074fc73df1862562d7d48c8850
Artem Hryhorov <a.hryhorov@geniusee.com>
2021-11-17 10:19:37 +0200

    Added navigation to crypto flow

--- android/app/src/main/java/updated/mysterium/vpn/ui/payment/method/PaymentMethodActivity.kt
+++ android/app/src/main/java/updated/mysterium/vpn/ui/payment/method/PaymentMethodActivity.kt
@@ -1,8 +1,13 @@
 package updated.mysterium.vpn.ui.payment.method
 
+import android.content.Intent
 import android.os.Bundle
+import network.mysterium.vpn.R
 import network.mysterium.vpn.databinding.ActivityPaymentMethodBinding
+import org.koin.android.ext.android.inject
 import updated.mysterium.vpn.ui.base.BaseActivity
+import updated.mysterium.vpn.ui.top.up.crypto.TopUpCryptoActivity
+import updated.mysterium.vpn.ui.wallet.ExchangeRateViewModel
 
 class PaymentMethodActivity : BaseActivity() {
 
@@ -11,10 +16,39 @@ class PaymentMethodActivity : BaseActivity() {
     }
 
     private lateinit var binding: ActivityPaymentMethodBinding
+    private val exchangeRateViewModel: ExchangeRateViewModel by inject()
+    private var cryptoAmount: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPaymentMethodBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        bind()
+        getCryptoValue()
+    }
+
+    private fun bind() {
+        binding.cryptoPayment.setOnClickListener {
+            navigateToCryptoPaymentFlow()
+        }
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun getCryptoValue() {
+        cryptoAmount = intent.extras?.getInt(TopUpCryptoActivity.CRYPTO_AMOUNT_EXTRA_KEY)
+        cryptoAmount?.let {
+            binding.usdEquivalentTextView.text = getString(
+                R.string.top_up_usd_equivalent, exchangeRateViewModel.usdEquivalent * it
+            )
+        }
+    }
+
+    private fun navigateToCryptoPaymentFlow() {
+        val intent = Intent(this, TopUpCryptoActivity::class.java).apply {
+            putExtra(TopUpCryptoActivity.CRYPTO_AMOUNT_EXTRA_KEY, cryptoAmount)
+        }
+        startActivity(intent)
     }
 }
