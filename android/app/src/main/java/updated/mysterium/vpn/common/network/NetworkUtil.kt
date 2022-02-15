81edf1257522b69777a3beffd70034dfb1b14f0b
I.Tsymbaliuk <I.Tsymbaliuk@geniusee.com>
2022-02-15 17:21:32 +0300

    - Added TAG extension - Captured ConnectivityManager exceptions

--- android/app/src/main/java/updated/mysterium/vpn/common/network/NetworkUtil.kt
+++ android/app/src/main/java/updated/mysterium/vpn/common/network/NetworkUtil.kt
@@ -3,19 +3,31 @@ package updated.mysterium.vpn.common.network
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.util.Log
+import updated.mysterium.vpn.common.extensions.TAG
 
 object NetworkUtil {
 
     fun isNetworkAvailable(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network = connectivityManager.activeNetwork ?: return false
-        val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
-        return when {
-            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
-            else -> false
+        try {
+            val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val network = connectivityManager.activeNetwork ?: return false
+            val networkCapabilities =
+                connectivityManager.getNetworkCapabilities(network) ?: return false
+            return when {
+                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        } catch (exception: SecurityException) {
+            Log.e(TAG, "Get network status failed with $exception")
+            return false
+        } catch (exception: RuntimeException) {
+            Log.e(TAG, "Get network status failed with $exception")
+            return false
         }
     }
 }
