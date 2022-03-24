353822354ee73df95bccb06686b28c6d2289b5b8
IrynaTsymbaliuk <Iryna.Tsymbaliuk.dev@gmail.com>
2022-03-24 04:21:23 +0400

    Removed webview

--- android/app/src/main/java/updated/mysterium/vpn/ui/top/up/card/summary/CardSummaryActivity.kt
+++ android/app/src/main/java/updated/mysterium/vpn/ui/top/up/card/summary/CardSummaryActivity.kt
@@ -1,26 +1,20 @@
 package updated.mysterium.vpn.ui.top.up.card.summary
 
-import android.animation.ObjectAnimator
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import network.mysterium.vpn.R
 import network.mysterium.vpn.databinding.ActivityCardSummaryBinding
-import network.mysterium.vpn.databinding.PopUpCardPaymentBinding
 import org.koin.android.ext.android.inject
+import updated.mysterium.vpn.common.extensions.TAG
 import updated.mysterium.vpn.model.payment.CardOrder
 import updated.mysterium.vpn.model.payment.PaymentStatus
 import updated.mysterium.vpn.model.pushy.PushyTopic
-import updated.mysterium.vpn.notification.PaymentStatusService
 import updated.mysterium.vpn.ui.base.BaseActivity
 import updated.mysterium.vpn.ui.home.selection.HomeSelectionActivity
 import updated.mysterium.vpn.ui.top.up.PaymentStatusViewModel
 import updated.mysterium.vpn.ui.top.up.coingate.payment.TopUpPaymentViewModel
-
 
 class CardSummaryActivity : BaseActivity() {
 
@@ -28,17 +22,12 @@ class CardSummaryActivity : BaseActivity() {
         const val CRYPTO_AMOUNT_EXTRA_KEY = "CRYPTO_AMOUNT_EXTRA_KEY"
         const val CRYPTO_CURRENCY_EXTRA_KEY = "CRYPTO_CURRENCY_EXTRA_KEY"
         const val COUNTRY_EXTRA_KEY = "COUNTRY_EXTRA_KEY"
-        private const val TAG = "CardSummaryActivity"
-        private const val HTML_MIME_TYPE = "text/html"
-        private const val ENCODING = "utf-8"
-        private const val paymentCallbackUrl = "https://checkout.cardinity.com/callback/"
     }
 
     private lateinit var binding: ActivityCardSummaryBinding
     private val viewModel: CardSummaryViewModel by inject()
     private val paymentViewModel: TopUpPaymentViewModel by inject()
     private val paymentStatusViewModel: PaymentStatusViewModel by inject()
-    private var paymentHtml: String? = null
     private var paymentProcessed = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +37,7 @@ class CardSummaryActivity : BaseActivity() {
         subscribeViewModel()
         bind()
         getMystAmount()
-        loadPayment()
+        loadPaymentData()
     }
 
     private fun subscribeViewModel() {
@@ -64,42 +53,10 @@ class CardSummaryActivity : BaseActivity() {
             finish()
         }
         binding.confirmButton.setOnClickListener {
-            launchCardinityPayment()
-        }
-        binding.closeButton.setOnClickListener {
-            binding.closeButton.visibility = View.GONE
-            binding.webView.visibility = View.GONE
-            if (paymentProcessed) {
-                showPaymentPopUp()
-                paymentProcessed = false
-            }
+
         }
         binding.paymentProcessingLayout.closeBannerButton.setOnClickListener {
             binding.paymentProcessingLayout.root.visibility = View.GONE
-        }
-    }
-
-    private fun showPaymentPopUp() {
-        val bindingPopUp = PopUpCardPaymentBinding.inflate(layoutInflater)
-        val dialog = createPopUp(bindingPopUp.root, false)
-        bindingPopUp.okayButton.setOnClickListener {
-            dialog.dismiss()
-            showPaymentProcessingBanner()
-        }
-        dialog.show()
-    }
-
-    private fun showPaymentProcessingBanner() {
-        binding.paymentProcessingLayout.root.visibility = View.VISIBLE
-        val animationX =
-            (binding.titleTextView.x + binding.titleTextView.height + resources.getDimension(R.dimen.margin_padding_size_medium))
-        ObjectAnimator.ofFloat(
-            binding.paymentProcessingLayout.root,
-            "translationY",
-            animationX
-        ).apply {
-            duration = 2000
-            start()
         }
     }
 
@@ -110,22 +67,10 @@ class CardSummaryActivity : BaseActivity() {
         )
     }
 
-    private fun loadPayment() {
+    private fun loadPaymentData() {
         val amount = intent.extras?.getInt(CRYPTO_AMOUNT_EXTRA_KEY) ?: return
         val currency = intent.extras?.getString(CRYPTO_CURRENCY_EXTRA_KEY) ?: return
         val country = intent.extras?.getString(COUNTRY_EXTRA_KEY) ?: return
-
-        startService()
-
-        paymentStatusViewModel.getPayment(amount, country, currency).observe(this) {
-            it.onSuccess { order ->
-                inflateOrderData(order)
-                paymentHtml = order.pageHtml.htmlSecureData.toString()
-            }
-            it.onFailure { error ->
-                Log.e(TAG, error.localizedMessage ?: error.toString())
-            }
-        }
     }
 
     private fun inflateOrderData(cardOrder: CardOrder) {
@@ -137,25 +82,6 @@ class CardSummaryActivity : BaseActivity() {
         binding.vatTextView.text = getString(
             R.string.card_payment_vat_value, taxesPercent
         )
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun launchCardinityPayment() {
-        paymentHtml?.let { htmlData ->
-            binding.closeButton.visibility = View.VISIBLE
-            binding.webView.visibility = View.VISIBLE
-            binding.webView.settings.javaScriptEnabled = true
-            binding.webView.webViewClient = object : WebViewClient() {
-
-                override fun onLoadResource(view: WebView?, url: String?) {
-                    super.onLoadResource(view, url)
-                    if (url?.contains(paymentCallbackUrl) == true) {
-                        paymentProcessed = true
-                    }
-                }
-            }
-            binding.webView.loadDataWithBaseURL(null, htmlData, HTML_MIME_TYPE, ENCODING, null)
-        }
     }
 
     private fun paymentConfirmed() {
@@ -190,9 +116,5 @@ class CardSummaryActivity : BaseActivity() {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
         }
         startActivity(intent)
-    }
-
-    private fun startService() {
-        startService(Intent(this, PaymentStatusService::class.java))
     }
 }
