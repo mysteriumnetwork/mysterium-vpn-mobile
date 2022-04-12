a05f78ea0c71969e43d440235cf92eef01fa76a9
IrynaTsymbaliuk <Iryna.Tsymbaliuk.dev@gmail.com>
2022-04-12 12:33:31 +0400

    Fixed card corner radius

--- android/app/src/main/java/updated/mysterium/vpn/model/payment/GatewayCardItem.kt
+++ android/app/src/main/java/updated/mysterium/vpn/model/payment/GatewayCardItem.kt
@@ -8,35 +8,34 @@ import network.mysterium.vpn.R
 data class GatewayCardItem(
     @StringRes val titleId: Int,
     @DrawableRes val iconId: Int,
-    @ColorRes val backgroundId: Int,
+    @DrawableRes val backgroundId: Int,
     @ColorRes val textColorId: Int,
     @ColorRes val iconColorId: Int
 ) {
     companion object {
-        fun from(value: Gateway): GatewayCardItem? {
+        fun from(value: Gateway): GatewayCardItem {
             return when (value) {
                 Gateway.COINGATE -> GatewayCardItem(
                     R.string.crypto,
                     R.drawable.icon_crypto,
-                    R.color.payment_method_crypto_background,
+                    R.drawable.shape_rectangle_gray_rounded_10,
                     R.color.payment_method_crypto_text_color,
                     R.color.payment_method_crypto_icon_color
                 )
                 Gateway.PLAY_BILLING -> GatewayCardItem(
                     R.string.credit_card,
                     R.drawable.icon_top_up_now,
-                    R.color.payment_method_card_background,
+                    R.drawable.shape_rectangle_pink_rounded_10,
                     R.color.payment_method_card_text_color,
                     R.color.payment_method_card_icon_color
                 )
                 Gateway.PAYPAL -> GatewayCardItem(
                     R.string.paypal,
                     R.drawable.icon_paypal,
-                    R.color.payment_method_paypal_background,
+                    R.drawable.shape_rectangle_blue_rounded_10,
                     R.color.payment_method_paypal_text_color,
                     R.color.payment_method_paypal_icon_color
                 )
-                else -> null
             }
         }
     }
