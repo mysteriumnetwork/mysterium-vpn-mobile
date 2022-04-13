498b99746e8e5318f6b56aaf68efc7e2856bc1ae
IrynaTsymbaliuk <Iryna.Tsymbaliuk.dev@gmail.com>
2022-04-13 16:04:59 +0400

    Fixed payment method item design

--- android/app/src/main/java/updated/mysterium/vpn/model/payment/GatewayCardItem.kt
+++ android/app/src/main/java/updated/mysterium/vpn/model/payment/GatewayCardItem.kt
@@ -17,21 +17,21 @@ data class GatewayCardItem(
             return when (value) {
                 Gateway.COINGATE -> GatewayCardItem(
                     R.string.crypto,
-                    R.drawable.icon_crypto,
+                    R.drawable.icon_crypto_payment_method,
                     R.drawable.shape_rectangle_gray_rounded_10,
                     R.color.payment_method_crypto_text_color,
                     R.color.payment_method_crypto_icon_color
                 )
                 Gateway.PLAY_BILLING -> GatewayCardItem(
                     R.string.credit_card,
-                    R.drawable.icon_top_up_now,
-                    R.drawable.shape_rectangle_pink_rounded_10,
+                    R.drawable.ic_card_payment_method,
+                    R.drawable.shape_rectangle_light_pink_rounded_10,
                     R.color.payment_method_card_text_color,
                     R.color.payment_method_card_icon_color
                 )
                 Gateway.PAYPAL -> GatewayCardItem(
                     R.string.paypal,
-                    R.drawable.icon_paypal,
+                    R.drawable.icon_paypal_payment_method,
                     R.drawable.shape_rectangle_blue_rounded_10,
                     R.color.payment_method_paypal_text_color,
                     R.color.payment_method_paypal_icon_color
