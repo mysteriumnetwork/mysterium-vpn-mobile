770b5847a70a43fabf50ce47be137a8afbc8a10f
IrynaTsymbaliuk <Iryna.Tsymbaliuk.dev@gmail.com>
2022-07-12 19:04:56 +0200

    Removed show review in play store fun from crypto flavor

---
+++ android/app/src/crypto/java/updated/mysterium/vpn/common/ShowReview.kt
@@ -0,0 +1,9 @@
+package updated.mysterium.vpn.common
+
+import android.app.Activity
+import android.util.Log
+import updated.mysterium.vpn.common.extensions.TAG
+
+fun Activity.showReview() {
+    Log.e(TAG, "Showing review is unavailable for this flavor")
+}
