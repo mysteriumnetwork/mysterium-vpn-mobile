e7ba2d8e75a4cbc2c9607c29f56d63bf975a45b0
IrynaTsymbaliukGeniusee <i.tsymbaliuk@geniusee.com>
2022-08-01 15:41:18 +0300

    Added gatewayClientCallback

--- android/app/src/playstore/java/updated/mysterium/vpn/ui/top/up/play/billing/summary/PlayBillingSummaryViewModel.kt
+++ android/app/src/playstore/java/updated/mysterium/vpn/ui/top/up/play/billing/summary/PlayBillingSummaryViewModel.kt
@@ -1,12 +1,20 @@
 package updated.mysterium.vpn.ui.top.up.play.billing.summary
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import mysterium.RegisterIdentityRequest
+import updated.mysterium.vpn.common.extensions.TAG
 import updated.mysterium.vpn.common.extensions.liveDataResult
 import updated.mysterium.vpn.common.livedata.SingleLiveEvent
+import updated.mysterium.vpn.model.payment.Gateway
 import updated.mysterium.vpn.model.payment.PaymentStatus
 import updated.mysterium.vpn.model.payment.PlayBillingOrderRequestInfo
+import updated.mysterium.vpn.model.payment.Purchase
 import updated.mysterium.vpn.model.wallet.IdentityModel
 import updated.mysterium.vpn.network.provider.usecase.UseCaseProvider
 
@@ -24,6 +32,29 @@ class PlayBillingSummaryViewModel(
     private val connectionUseCase = useCaseProvider.connection()
     private val loginUseCase = useCaseProvider.login()
     private val paymentUseCase = useCaseProvider.payment()
+
+    init {
+        subscribeOnBillingFlow()
+    }
+
+    private fun subscribeOnBillingFlow() {
+        try {
+            playBillingDataSource.purchaseConsumedFlow
+                .distinctUntilChanged()
+                .onEach {
+                    val purchase = Purchase(
+                        identityAddress = connectionUseCase.getIdentityAddress(),
+                        gateway = Gateway.GOOGLE,
+                        googlePurchaseToken = it.purchaseToken,
+                        googleProductID = it.skus.first()
+                    )
+                    paymentUseCase.gatewayClientCallback(purchase)
+                }
+                .launchIn(viewModelScope)
+        } catch (exception: Throwable) {
+            Log.e(TAG, exception.localizedMessage ?: exception.toString())
+        }
+    }
 
     fun getPlayBillingPayment(
         info: PlayBillingOrderRequestInfo
