098c46c4f333394dd448c8ee5051b6247b98f464
Artem Hryhorov <a.hryhorov@geniusee.com>
2022-08-11 14:26:39 +0300

    Fix interface implementation inject

--- android/app/src/playstore/java/updated/mysterium/vpn/di/FlavorModules.kt
+++ android/app/src/playstore/java/updated/mysterium/vpn/di/FlavorModules.kt
@@ -4,6 +4,8 @@ import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import updated.mysterium.vpn.common.PlayStoreHelperImpl
+import updated.mysterium.vpn.common.playstore.NotificationsHelper
+import updated.mysterium.vpn.common.playstore.PlayStoreHelper
 import updated.mysterium.vpn.notification.Notifications
 import updated.mysterium.vpn.ui.top.up.play.billing.amount.usd.PlayBillingAmountUsdViewModel
 import updated.mysterium.vpn.ui.top.up.play.billing.summary.PlayBillingDataSource
@@ -21,10 +23,10 @@ object FlavorModules {
         viewModel {
             PlayBillingSummaryViewModel(get(), get())
         }
-        single {
+        single<PlayStoreHelper> {
             PlayStoreHelperImpl()
         }
-        single {
+        single<NotificationsHelper> {
             Notifications(androidApplication())
         }
     }
