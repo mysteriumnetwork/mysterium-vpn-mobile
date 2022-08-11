204d855d39204bd6d1383ae3e7ab55a43f74b180
Artem Hryhorov <a.hryhorov@geniusee.com>
2022-08-11 18:11:16 +0300

    Error handling refactoring

--- android/app/src/main/java/updated/mysterium/vpn/exceptions/BaseNetworkException.kt
+++ android/app/src/main/java/updated/mysterium/vpn/exceptions/BaseNetworkException.kt
@@ -7,50 +7,12 @@ open class BaseNetworkException(val exception: Exception) : Exception() {
 
     companion object {
         fun fromException(exception: Exception): BaseNetworkException {
-            return BaseNetworkException(handleException(exception))
+            return BaseNetworkException(ErrorHandler.getException(exception))
         }
 
         fun fromErrorCode(errorCode: String, errorMessage: String): BaseNetworkException {
-            return BaseNetworkException(handleFromErrorCode(errorCode, errorMessage))
+            return BaseNetworkException(ErrorHandler.getException(errorCode, errorMessage))
         }
-
-        private fun handleFromErrorCode(errorCode: String, errorMessage: String): Exception {
-            return when (errorCode) {
-                "InvalidProposal" -> ConnectInvalidProposalException(errorMessage)
-                "InsufficientBalance" -> ConnectInsufficientBalanceException(errorMessage)
-                else -> {
-                    if (errorMessage == "connection already exists") {
-                        throw ConnectAlreadyExistsException(errorMessage)
-                    } else {
-                        throw ConnectUnknownException(errorMessage)
-                    }
-                }
-            }
-        }
-
-        private fun handleException(exception: Exception): Exception {
-            return if (exception.message?.contains(NO_BALANCE_ERROR_MESSAGE) == true) {
-                TopupNoAmountException()
-            } else if (isBalanceLimitException(exception.message.toString())) {
-                TopupBalanceLimitException(getBalanceLimit(exception.message.toString()))
-            } else error(exception)
-        }
-
-        private fun isBalanceLimitException(exceptionMessage: String?): Boolean {
-            return exceptionMessage?.let {
-                Regex(BALANCE_LIMIT_ERROR_REGEX_PATTERN).containsMatchIn(it)
-            } ?: false
-        }
-
-        private fun getBalanceLimit(exceptionMessage: String?): Double {
-            return exceptionMessage?.let {
-                Regex("\\d+\\.\\d{2}").find(it)?.value?.toDouble()
-            } ?: 5.0
-        }
-
-        private val BALANCE_LIMIT_ERROR_REGEX_PATTERN =
-            "You can only top-up if you have less than \\d+.\\d{2} MYST in balance"
-        private const val NO_BALANCE_ERROR_MESSAGE = "Cannot provide more balance at this time"
     }
 
     fun getMessage(context: Context): String {
@@ -62,5 +24,4 @@ open class BaseNetworkException(val exception: Exception) : Exception() {
             else -> localizedMessage ?: context.getString(R.string.unknown_error)
         }
     }
-
 }
