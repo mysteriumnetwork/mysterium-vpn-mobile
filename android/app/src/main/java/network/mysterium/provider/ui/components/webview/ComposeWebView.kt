389c301e091f4526aa1cb82a57dcc76258053c3a
Ivan Podibka <i.podibka@geniusee.com>
2023-05-14 04:51:33 +0300

    - Fixed displaying NodeUI in webview

--- android/app/src/main/java/network/mysterium/provider/ui/components/webview/ComposeWebView.kt
+++ android/app/src/main/java/network/mysterium/provider/ui/components/webview/ComposeWebView.kt
@@ -3,10 +3,13 @@ package network.mysterium.provider.ui.components.webview
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
@@ -15,7 +18,8 @@ fun ComposeWebView(
     url: String
 ) {
     AndroidView(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxSize(),
         factory = {
             WebView(it).apply {
                 layoutParams = ViewGroup.LayoutParams(
--- android/app/src/main/java/network/mysterium/provider/ui/screens/nodeui/NodeUIScreen.kt
+++ android/app/src/main/java/network/mysterium/provider/ui/screens/nodeui/NodeUIScreen.kt
@@ -1,12 +1,20 @@
 package network.mysterium.provider.ui.screens.nodeui
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.toSize
 import network.mysterium.provider.ui.components.buttons.HomeButton
 import network.mysterium.provider.ui.components.buttons.SettingsButton
 import network.mysterium.provider.ui.components.content.LogoScreenContent
@@ -44,7 +52,6 @@ private fun NodeUIScreenContent(
         }
     ) {
         ComposeWebView(
-            modifier = Modifier.verticalScroll(rememberScrollState()),
             url = state.url
         )
     }
