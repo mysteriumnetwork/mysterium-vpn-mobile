98cc9dd55ce789d455108fc6cef123518ea4fd10
Ivan Podibka <i.podibka@geniusee.com>
2023-05-29 23:30:03 +0300

    - Decreased mobile data limit

--- android/app/src/main/java/network/mysterium/provider/Config.kt
+++ android/app/src/main/java/network/mysterium/provider/Config.kt
@@ -1,7 +1,7 @@
 package network.mysterium.provider
 
 object Config {
-    const val minMobileDataLimit = 50
+    const val minMobileDataLimit = 1
     const val maxMobileDataLimit = 999999
     const val payPalRedirectUrl = "https://pilvytis.mysterium.network/api/v2/payment/paypal/redirect"
     const val stripeRedirectUrl = "https://pilvytis.mysterium.network/api/v2/payment/stripe/redirect"
