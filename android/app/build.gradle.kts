e5b564fc98ea1ea7bd46c9fc37b811cf8fbbf568
geniuseework <i.kulikov@geniusee.com>
2023-11-01 14:43:52 +0200

    version pump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "network.mysterium.provider"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
