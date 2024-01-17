db2d9be0c50af17f1e0fac036bd52dcaa24495ca
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2024-01-17 13:00:07 +0200

    Release 1.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "network.mysterium.provider"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
