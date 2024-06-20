0855bfb1fbf83f2711cf0bfd6d3e9199d5493922
ArtemHryhorovGeniusee <a.hryhorov@geniusee.com>
2024-06-20 22:19:51 +0300

    Start screen UI fixes

--- android/app/src/main/java/network/mysterium/provider/ui/screens/start/StartScreen.kt
+++ android/app/src/main/java/network/mysterium/provider/ui/screens/start/StartScreen.kt
@@ -1,15 +1,22 @@
 package network.mysterium.provider.ui.screens.start
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -19,11 +26,11 @@ import androidx.compose.ui.unit.dp
 import network.mysterium.provider.R
 import network.mysterium.provider.ui.components.buttons.PrimaryButton
 import network.mysterium.provider.ui.components.buttons.PrimaryTextButton
-import network.mysterium.provider.ui.components.content.ScreenContent
 import network.mysterium.provider.ui.components.logo.HeaderLogo
 import network.mysterium.provider.ui.components.logo.HeaderLogoStyle
 import network.mysterium.provider.ui.navigation.NavigationDestination
 import network.mysterium.provider.ui.theme.Colors
+import network.mysterium.provider.ui.theme.Corners
 import network.mysterium.provider.ui.theme.MysteriumTheme
 import network.mysterium.provider.ui.theme.Paddings
 import network.mysterium.provider.ui.theme.TextStyles
@@ -35,28 +42,42 @@ fun StartScreen(
 ) {
     val configuration = LocalConfiguration.current
     val screenHeight = configuration.screenHeightDp.dp
-    ScreenContent(
-        header = {
-            Header(modifier = Modifier.height(screenHeight * 0.6f))
-        },
+    val backgroundShape = RoundedCornerShape(
+        topStart = Corners.card,
+        topEnd = Corners.card
+    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
     ) {
-        Content(onNavigate = onNavigate)
+        Header(modifier = Modifier.fillMaxWidth())
+        Box(
+            modifier = Modifier
+                .height(screenHeight * 0.3f)
+                .clip(backgroundShape)
+                .background(
+                    color = Colors.primaryBg,
+                    shape = backgroundShape
+                )
+                .clipToBounds()
+        ) {
+            Content(onNavigate = onNavigate)
+        }
     }
 }
 
 @Composable
 private fun Header(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
     ) {
         HeaderLogo(
             style = HeaderLogoStyle.Big,
             modifier = Modifier.padding(top = 24.dp),
         )
-        Spacer(modifier = Modifier.weight(1f))
-        HeaderText()
-        Spacer(modifier = Modifier.weight(1f))
+        HeaderText(modifier = Modifier.padding(vertical = 16.dp))
         HeaderDescription()
     }
 }
@@ -67,8 +88,9 @@ private fun Content(
     viewModel: StartScreenViewModel = getViewModel(),
 ) {
     Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
     ) {
         PrimaryButton(
             modifier = Modifier
@@ -81,9 +103,7 @@ private fun Content(
                 onNavigate(NavigationDestination.Settings(true))
             }
         )
-        Spacer(modifier = Modifier.weight(1f))
         PrimaryTextButton(
-            modifier = Modifier.padding(Paddings.default),
             text = stringResource(id = R.string.terms_and_conditions),
             color = Colors.grey500,
             onClick = {
@@ -94,8 +114,11 @@ private fun Content(
 }
 
 @Composable
-private fun HeaderText() {
+private fun HeaderText(
+    modifier: Modifier = Modifier,
+) {
     Text(
+        modifier = modifier,
         text = stringResource(id = R.string.earn_while_you_sleep),
         style = TextStyles.splashHeader,
         color = Colors.blue700,
